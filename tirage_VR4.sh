f4fd19d4bc9c9e18747350f73c300b5e9e300ed8
rungis86 <web.remi@verschaeve-de-sousa.fr>
2022-11-24 09:56:51 +0100

    Correction coquille tirage_VR4.sh

--- tirage_VR4.sh
+++ tirage_VR4.sh
@@ -13,7 +13,7 @@ Ce script permet de réaliser un tirage au sort aléatoire de 10 sauts de VR4.
 Vous aurez la possibilité de choisir les règles N1 ou N2.
 
 N1 >> tous les blocs sont présents dans le tirage, chaque manche compte
-4 ou 5 points.
+5 ou 6 points.
 N2 >> les blocs N1 ne sont pas présents, chaque manche compte 4 ou 5 points.
 
 ############################################################################
@@ -138,9 +138,17 @@ tri_fichier_liste_figures(){
 }
 
 sortie(){
-cat <<EOF
+
+if [ ! "$1" ]; then
+
+	cat <<EOF
 
 ###### Tirage au sort VR4 N$categorie ######
+EOF
+
+fi
+
+cat <<EOF
 
 ${tete_colonne[1]}	${tete_colonne[2]}	${tete_colonne[3]}	${tete_colonne[4]}	${tete_colonne[5]}	${tete_colonne[6]}	${tete_colonne[7]}	${tete_colonne[8]}	${tete_colonne[9]}	${tete_colonne[10]}	${tete_colonne[11]}
 ${figure[1]}	${figure[7]}	${figure[13]}	${figure[19]}	${figure[25]}	${figure[31]}	${figure[37]}	${figure[43]}	${figure[49]}	${figure[55]}	${figure[61]}
@@ -149,7 +157,6 @@ ${figure[3]}	${figure[9]}	${figure[15]}	${figure[21]}	${figure[27]}	${figure[33]
 ${figure[4]}	${figure[10]}	${figure[16]}	${figure[22]}	${figure[28]}	${figure[34]}	${figure[40]}	${figure[46]}	${figure[52]}	${figure[58]}	${figure[64]}
 ${figure[5]}	${figure[11]}	${figure[17]}	${figure[23]}	${figure[29]}	${figure[35]}	${figure[41]}	${figure[47]}	${figure[53]}	${figure[59]}	${figure[65]}
 ${figure[6]}	${figure[12]}	${figure[18]}	${figure[24]}	${figure[30]}	${figure[36]}	${figure[42]}	${figure[48]}	${figure[54]}	${figure[60]}	${figure[66]}
-
 EOF
 }
 
@@ -163,7 +170,7 @@ main(){
 	init_tetes_colonnes
 	boucle
 	tri_fichier_liste_figures
-	sortie
+	sortie "$1"
 }
 
 main "$1" "$2"
